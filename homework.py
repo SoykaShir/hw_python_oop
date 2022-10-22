5e03970eb8777bdaf9c0c9eeb28692e0f7769341
Широкова Сойка <mayerfox@mail.ru>
2022-10-22 20:50:09 +0300

    my first project type

--- homework.py
+++ homework.py
@@ -108,7 +108,7 @@ class Swimming(Training):
                 * self.duration)
 
 
-def read_package(workout_type: str, data: list) -> Training:
+def read_package(workout_type: str, data: list[int]) -> Training:
     """Прочитать данные полученные от датчиков."""
     training_type: dict[str, type[Training]] = {'RUN': Running,
                                                 'WLK': SportsWalking,
